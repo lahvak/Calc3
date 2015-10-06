e0a1bf9250c6c88a1893b11968db3c74e2779a8c
lahvak <jhlavace@svsu.edu>
2015-10-06 00:30:32 -0400

    Added TeX explanations and compiled movie

--- curve/makemovie.py
+++ curve/makemovie.py
@@ -1,39 +1,33 @@
 from moviepy.editor import *
 
-#Edit this stuff:
-
 title = (ImageClip("TeX/static_dark_frames_000.png")
-                 .set_duration(3)
-                 .fadeout(1))
-intro1 = (ImageClip("TeX/static_frames_000.png")
-                 .set_duration(3)
-                 .fadein(1))
-intro2 = (ImageClip("TeX/static_frames_001.png")
-                 .set_duration(3)
+                 .set_duration(4)
                  .fadeout(1))
 
-density = [ImageClip("TeX/static_frames_{:03d}.png".format(i)).set_duration(5) for i in [2,3,4]]
-contours = ImageClip("TeX/static_frames_005.png").set_duration(8).fadeout(1)
-limits = [ImageClip("TeX/static_frames_{:03d}.png".format(i)).set_duration(5) for i in [6,7,8,9,10,11]]
-ending = [ImageClip("TeX/static_frames_{:03d}.png".format(i)).set_duration(5) for i in [12,13,14]]
-density[0] = density[0].fadein(1)
-limits[0] = limits[0].fadein(1)
-limits[-1] = limits[-1].fadeout(1)
-ending[2] = ending[2].fadeout(1)
+frame = [ImageClip("TeX/static_frames_{:03d}.png".format(i)).set_duration(4) for i in range(6)]
+circle = [ImageClip("TeX/static_frames_{:03d}.png".format(i)).set_duration(4) for i in [6,7,8]]
+coaster = [ImageClip("TeX/static_frames_{:03d}.png".format(i)).set_duration(4) for i in [9,10,11]]
+frame[0] = frame[0].fadein(1)
+frame[-1] = frame[-1].fadeout(1)
+circle[0] = circle[0].fadein(1)
+circle[-1] = circle[-1].fadeout(1)
+coaster[0] = coaster[0].fadein(1)
+coaster[-1] = coaster[-1].fadeout(1)
 
-flyintro = (ImageClip("TeX/static_dark_frames_001.png")
-                 .set_duration(2)
-                 .fadein(1)
-                 .fadeout(1))
-flyaround = VideoFileClip("povray/flyaround.avi")
-totop = VideoFileClip("povray/topview.avi")
-twodcont = VideoFileClip("TeX/anim_curves.avi")
-aftertwodcont = (ImageClip("TeX/after_anim_curves_000.png")
-                 .set_duration(3)
-                 .fadeout(1))
-threedcont1 = VideoFileClip("povray/progress.avi")
-threedcont2 = VideoFileClip("povray/progress_second.avi")
+curve_movie = VideoFileClip("povray/curve.avi")
+circle_movie = VideoFileClip("povray/circle.avi")
+coaster_movie = VideoFileClip("povray/coaster.avi")
 
-comp = concatenate([title,intro1,intro2]+density+[contours,flyintro,flyaround,totop]+limits+[twodcont,aftertwodcont,threedcont1,threedcont2]+ending)
+w,h = movie_size = coaster_movie.size
 
-comp.write_videofile("all.mp4")
+small_coaster = (VideoFileClip("povray/circle.avi").
+                 resize((w/3,h/3)).
+                 margin(4,color=(100,100,100)).
+                 margin(bottom=10,right=10,opacity=0).
+                 set_pos(('right','bottom')))
+
+coaster_comp = CompositeVideoClip([coaster_movie, small_coaster])
+
+comp = concatenate([title]+frame+[curve_movie]+circle+[circle_movie]+coaster+[coaster_comp])
+
+comp.write_videofile("tnb.mp4")
